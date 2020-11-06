16a317f76c8a0c2f12fa938eaf6eb407e04b85c0
Fernando R. Guzman Casas <fernando.guzman2@upr.edu>
2020-11-05 23:50:48 -0400

    User routes implemented

--- RestAPI/src/routes/user.js
+++ RestAPI/src/routes/user.js
@@ -1,21 +1,11 @@
 
 const { Router } = require('express');
 const { nodemailer } = require('nodemailer');
+const { _ } = require('underscore');
+const users = require('../sample.json');
 const router = Router();
 
 
-
-router.get('/', (req, res) =>{
-    res.send(`Tu Camino Empresarial`);
-});
-
-router.get('/home', (req, res) => {
-    res.json({"Titulo": "Hello World"})
-});
-
-router.get('/user/:userId', (req,res) => {
-
-});
 
 
 
@@ -46,21 +36,6 @@ router.post('/register', (req, res) => {
     
 });
 
-// Validates email address of course.
-function validEmail(email) {
-    var filter = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
-    return String(email).search (filter) != -1;
-}
-
-function validPhone(phone) {
-    var filter = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-    return String(phone).search (filter) != -1;
-}
-
-function validName(name) {
-    var filter = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/
-    return String(name).search (filter) != -1;
-}
 
 
 router.post('/login', (req,res) => {
@@ -77,15 +52,96 @@ router.post('/login', (req,res) => {
     }
 });
 
-router.put('/user', (req, res) => {
-    //res.json({"Titulo": "Hello World"})
-    res.send();
-    console.log(req.test);
+router.get('/user/:userId', (req,res) => {
+    //console.log(req.params);
+    user = {
+        "id" : "1",
+        "firstname" : "juan1",
+        "lastname" : "del pueblo",
+        "business_status" : "activo",
+        "email" : "test@test.com",
+        "phone_number" : "123-456-9999",
+        "requested_assistance" : "money",
+        "password" : "password"
+    }
+
+    if (user){
+        res.status(200).json(user);
+    }
+    else{
+        res.status(404).send("User not found");
+    }
+
+});
+
+router.put('/user/:userId', (req, res) => {
+    
+    const {firstname, lastname, business_status, phone_number} = req.body;
+    
+    if (firstname && lastname && business_status ){
+      
+        if(phone_number){
+            
+            if(validName(firstname) && validName(lastname) && validPhone(phone_number)){
+                
+                _.each(users, (user, i) => {
+                    
+                    if(user.id == req.params.userId){
+                        user.firstname = firstname;
+                        user.lastname = lastname;
+                        user.business_status = business_status;
+                        user.phone_number = phone_number;
+
+                    }
+                });
+                console.log("Update user: " + req.params.userId);
+                res.status(200).json(users);
+            }
+            else{
+                res.status(400).send("Error");
+            }
+        }
+        
+    }
+    else{
+        res.status(400).send("Error");
+    }
 });
 
 
-router.delete('user/:userId', (req,res) => {
 
+router.delete('/user/:userId', (req,res) => {
+    let isDeleted = false;
+    _.each(users, (user, i) => {            
+        if(user.id == req.params.userId){
+            users.splice(i,1);
+            isDeleted = true;
+        }
+    });
+
+    if(isDeleted){
+        res.status(200).json(users);
+    }
+    else {
+        res.status(404).send('User not found.');
+    }
+    //res.status(404).send("User not found.");
 });
+
+// Validates email address of course.
+function validEmail(email) {
+    var filter = /^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/;
+    return String(email).search (filter) != -1;
+}
+
+function validPhone(phone) {
+    var filter = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return String(phone).search (filter) != -1;
+}
+
+function validName(name) {
+    var filter = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/
+    return String(name).search (filter) != -1;
+}
 
 module.exports = router;
