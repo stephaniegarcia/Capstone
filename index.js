43f684b51ae46bffef47f3b531f587e07563d8f8
Stephanie Garcia <sgribot19@gmail.com>
2020-11-30 01:42:30 -0400

    deploy try 2

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //settings
 app.set('port', process.env.API_PORT || 3030);
@@ -26,6 +28,10 @@ app.use(require('./routes/questions'));
 app.use(require('./routes/businessType'));
 app.use(require('./routes/businessStep'));
 app.use(require('./routes/businessStage'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 //starting the server
 app.listen(app.get('port'), () => {
